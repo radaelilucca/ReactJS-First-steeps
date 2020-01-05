9a8d2d1d67cceb3893d0be7a1aa782a4f8a936e3
radaelilucca <luccaradaeli@gmail.com>
2020-01-05 06:03:15 -0300

    Estados e Imutabildiade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,21 +3,49 @@ import React, { Component } from 'react'
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: [
       'NodeJS',
       'ReactJS',
-      'ReactNative'
+      'React Native'
     ]
   };
 
+  handleInputChange = e => {
+    this.setState({
+      newTech: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    })
+
+  }
+
   render(){
-    console.log(this.state)
+  
     return (
-      <ul>
-        <li>Node.JS</li>
-        <li>ReactJs</li>
-        <li>React Native</li>
-      </ul>
+      <form 
+      onSubmit={this.handleSubmit}>
+
+        <ul>{
+        this.state.techs.map(tech =>
+          <li key={tech}>{tech}</li>)
+        }</ul>
+
+        <input 
+        type="text" 
+        onChange={this.handleInputChange} 
+        value={this.state.newTech}
+        />
+        <button type="submit"> Enviar </button>
+
+      </form>
     )
   }
 }
