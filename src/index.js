c29148b86cc4b827ead0905dbd0666c7fa18befc
radaelilucca <luccaradaeli@gmail.com>
2020-01-05 05:14:36 -0300

    Creating root component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {render} from 'react-dom'
 
+const body = document.getElementById('app')
+
 
 import App from './App'
 
-render(<App />, document.getElementById('app'))
+render(<App />, body)
